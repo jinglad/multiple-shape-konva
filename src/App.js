d0f3ee80ea628393faef358e1f36923f328bef4a
Jihan Chowdhury <jihanchowdhury70@gmail.com>
2022-08-15 23:56:09 +0600

    testing multiple shape

--- src/App.js
+++ src/App.js
@@ -1,24 +1,210 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Layer, Rect, Stage, Transformer } from "react-konva";
+import { useEffect, useRef, useState } from "react";
+import Rectangle from "./Rectangle";
+import Konva from "konva";
+
+const initialRectangle = [
+  {
+    x: 10,
+    y: 10,
+    width: 100,
+    height: 100,
+    fill: "red",
+    id: "rect1",
+  },
+  {
+    x: 150,
+    y: 150,
+    width: 100,
+    height: 100,
+    fill: "green",
+    id: "rect2",
+  },
+];
 
 function App() {
+  const selectionRect = useRef(null);
+  const [rectangles, setRectangles] = useState(initialRectangle);
+  const [nodesArray, setNodes] = useState([]);
+  const trRef = useRef(null);
+  const [selectedId, selectShape] = useState(null);
+  const layerRef = useRef(null);
+  const selection = useRef({
+    visible: false,
+    x1: 0,
+    y1: 0,
+    x2: 0,
+    y2: 0,
+  });
+
+  const updateSelection = () => {
+    const node = selectionRect.current;
+    node.setAttrs({
+      visible: selection.current.visible,
+      x: Math.min(selection.current.x1, selection.current.x2),
+      y: Math.min(selection.current.y1, selection.current.y2),
+      width: Math.abs(selection.current.x1 - selection.current.x2),
+      height: Math.abs(selection.current.y1 - selection.current.y2),
+      fill: "rgba(0, 161, 255, 0.3)",
+    });
+  };
+
+  const onMouseDown = (e) => {
+    const isElement = e.target.findAncestor(".elements-container");
+    const isTransformer = e.target.findAncestor("Transformer");
+    if (isElement || isTransformer) {
+      return;
+    }
+    const pos = e.target.getStage().getPointerPosition();
+    selection.current.x1 = pos.x;
+    selection.current.y1 = pos.y;
+    selection.current.x2 = pos.x;
+    selection.current.y2 = pos.y;
+    selection.current.visible = true;
+    updateSelection();
+  };
+
+  const onMouseMove = (e) => {
+    if (!selection.current.visible) {
+      return;
+    }
+    const pos = e.target.getStage().getPointerPosition();
+    selection.current.x2 = pos.x;
+    selection.current.y2 = pos.y;
+    updateSelection();
+  };
+
+  const onMouseUp = (e) => {
+    if (!selection.current.visible) {
+      return;
+    }
+    const selBox = selectionRect.current.getClientRect();
+    const elements = [];
+    layerRef.current.find(".rectangle").forEach((node) => {
+      const nodeBox = node.getClientRect();
+      if (Konva.Util.haveIntersection(selBox, nodeBox)) {
+        elements.push(node);
+      }
+    });
+    trRef.current.nodes(elements);
+    selection.current.visible = false;
+    Konva.listenClickTap = false;
+    updateSelection();
+  };
+
+  const onClickTap = (e) => {
+    if (selection.current.visible) {
+      return;
+    }
+    let stage = e.target.getStage();
+    let layer = layerRef.current;
+    let tr = trRef.current;
+
+    console.log({tr})
+
+    if (e.target === stage) {
+      selectShape(null);
+      tr.nodes([]);
+      setNodes([]);
+      layer.batchDraw();
+      return;
+    }
+    if (!e.target.hasName("rectangle")) {
+      return;
+    }
+
+    const metaPressed = e.evt.ctrlKey || e.evt.shiftKey;
+    // console.log({nodesArray});
+    const isSelected = tr.nodes().indexOf(e.target) >= 0;
+    // console.log({isSelected})
+    // console.log(tr.nodes().indexOf(e.target));
+
+    // console.log(metaPressed, isSelected);
+
+    // if (!metaPressed && !isSelected) {
+    //   selectShape(e.target.id());
+    //   tr.nodes([e.target]);
+    //   setNodes([e.target]);
+      
+    // } else if (metaPressed && !isSelected) {
+    //   selectShape(e.target.id());
+    //   tr.nodes([...tr.nodes(), e.target]);
+    //   setNodes([...nodesArray, e.target]);
+      
+    // } else if (metaPressed && isSelected) {
+    //   selectShape(null);
+    //   const nodes = tr.nodes();
+    //   console.log(nodes);
+    //   const index = nodes.indexOf(e.target);
+    //   nodes.splice(index, 1);
+    //   setNodes(nodes);
+    //   tr.nodes(nodes);
+    // }
+
+    if (!metaPressed && !isSelected) {
+      // if no key pressed and the node is not selected
+      // select just one
+      tr.nodes([e.target]);
+    } else if (metaPressed && isSelected) {
+      // if we pressed keys and node was selected
+      // we need to remove it from selection:
+      const nodes = tr.nodes().slice(); // use slice to have new copy of array
+      // remove node from array
+      nodes.splice(nodes.indexOf(e.target), 1);
+      tr.nodes(nodes);
+    } else if (metaPressed && !isSelected) {
+      // add the node into selection
+      const nodes = tr.nodes().concat([e.target]);
+      tr.nodes(nodes);
+    }
+    trRef.current.getLayer().batchDraw();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stage
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onMouseDown={onMouseDown}
+      onMouseMove={onMouseMove}
+      onMouseUp={onMouseUp}
+      onClick={onClickTap}
+    >
+      <Layer ref={layerRef}>
+        {rectangles.map((rect, i) => (
+          <Rectangle
+            key={i}
+            shapeProps={rect}
+            isSelected={rect.id === selectedId}
+            onSelect={(e) => {
+              if (e.current !== undefined) {
+                let temp = nodesArray;
+                if (!nodesArray.includes(e.current)) temp.push(e.current);
+                setNodes(temp);
+                trRef.current.nodes(nodesArray);
+                trRef.current.getLayer().batchDraw();
+              }
+              selectShape(rect.id);
+            }}
+            onChange={(newAttrs) => {
+              let temp = rectangles;
+              temp[temp.findIndex((rect) => rect.id === selectedId)] = newAttrs;
+              setRectangles(temp);
+            }}
+          />
+        ))}
+        <Transformer
+          ref={trRef}
+          boundBoxFunc={(oldBox, newBox) => {
+            if (newBox.width < 5 || newBox.height < 5) {
+              return oldBox;
+            }
+            return newBox;
+          }}
+        />
+        <Rect fill="blue" ref={selectionRect} />
+      </Layer>
+    </Stage>
   );
 }
 
---
+++ src/Rectangle.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Rect } from 'react-konva';
+
+const Rectangle = ({shapeProps, onSelect, onChange}) => {
+  const shapeRef = React.useRef(null);
+  return (
+    <Rect
+      {...shapeProps}
+      onClick={() => onSelect(shapeRef)}
+      // onDragStart={() => onChange(shapeRef)}
+      onDragEnd={(e) => onChange({
+        ...shapeProps,
+        x: e.target.x(),
+        y: e.target.y(),
+      })}
+      ref={shapeRef}
+      onTap={() => onSelect(shapeRef)}
+      name="rectangle"
+      draggable
+      onTransformEnd={(e) => onChange({
+        ...shapeProps,
+        x: shapeRef.current.x(),
+        y: shapeRef.current.y(),
+        width: Math.max(5, shapeRef.current.width()*shapeRef.current.scaleX()),
+        height: Math.max(shapeRef.current.height() * shapeRef.current.scaleY(), 5),
+      })
+    }
+    />
+  )
+}
+
+export default Rectangle
